efa31ac6fc4af089a56eb73e232916bc25031120
shivamkumarpandit055-code <shivamkumarpandit055@gmail.com>
2025-08-12 20:24:48 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,8 +13,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://shivamkumar055gram:
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "77"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "774").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "7276272743"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "7276272743").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = "https://t.me/+W-Q51EuLf2QwYTl"
@@ -59,4 +59,5 @@ Please contact the admin @Radharadhe0 to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
